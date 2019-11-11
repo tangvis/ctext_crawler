bf3bcaba69b6459de8887f9227c724d26f58b97e
linshaofeng <123456>
2019-11-11 14:16:30 +0800

    init

---
+++ crawler.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@version: 0.1
+@author: linus
+@Email: linshaofeng1992@gmail.com
+@file: crawler.py
+@time: 2019/11/11 14:14
+"""
+import requests
---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@version: 0.1
+@author: linus
+@Email: linshaofeng1992@gmail.com
+@file: __init__.py.py
+@time: 2019/11/11 11:38
+"""
